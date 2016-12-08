f6f6d2b772de30afe57cbdc984c14c85c1e42af9
johndharrison <johndharrison0@gmail.com>
2016-12-08 07:12:37 -0800

    Refactor phantomjs test

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -7,7 +7,14 @@ mock_binman_list_versions_chrome <- function(...){
     win32 = c("2.23", "2.24", "2.25")
   )
 }
-
+mock_binman_list_versions_phantomjs <- function(...){
+  list(
+    linux32 = c("1.9.7", "1.9.8", "2.1.1"),
+    linux64 = c("1.9.7", "1.9.8", "2.1.1"),
+    macosx = c("1.9.8", "2.0.0", "2.1.1"),
+    windows = c("1.9.8", "2.0.0", "2.1.1")
+  )
+}
 mock_base_normalizePath <- function(path, winslash, mustWork){
   path
 }
--- tests/testthat/test-phantom.R
+++ tests/testthat/test-phantom.R
@@ -3,29 +3,13 @@ context("phantom")
 test_that("canCallPhantomJS", {
   with_mock(
     `binman::process_yaml` = function(...){},
-    `binman::list_versions` = function(...){
-      list(
-        linux32 = c("1.9.7", "1.9.8", "2.1.1"),
-        linux64 = c("1.9.7", "1.9.8", "2.1.1"),
-        macosx = c("1.9.8", "2.0.0", "2.1.1"),
-        windows = c("1.9.8", "2.0.0", "2.1.1")
-      )
-    },
-    `binman::app_dir` = function(...){
-      "some.dir"
-    },
-    `base::normalizePath` = function(path, winslash, mustWork){
-      path
-    },
-    `base::list.files` = function(...){
-      "some.path"
-    },
-    `subprocess::spawn_process` = function(...){
-      "hello"
-    },
-    `subprocess::process_return_code` = function(...){
-      NA
-    },
+    `binman::list_versions` = mock_binman_list_versions_phantomjs,
+    `binman::app_dir` = mock_binman_app_dir,
+    `base::normalizePath` = mock_base_normalizePath,
+    `base::list.files` = mock_base_list.files,
+    `subprocess::spawn_process` = mock_subprocess_spawn_process,
+    `subprocess::process_return_code` =
+      mock_subprocess_process_return_code,
     pDrv <- phantomjs()
   )
   expect_identical(pDrv$process, "hello")
