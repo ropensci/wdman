6ed96953ea873dd208c18d6209b44f263099bb8c
johndharrison <johndharrison0@gmail.com>
2016-12-15 10:04:01 -0800

    Update gecko driver

--- R/gecko.R
+++ R/gecko.R
@@ -79,8 +79,7 @@ gecko_check <- function(){
 gecko_ver <- function(platform, version){
   geckover <- binman::list_versions("geckodriver")[[platform]]
   geckover <- if(identical(version, "latest")){
-    vermax <- as.character(max(binman::sem_ver(gsub("v", "", geckover))))
-    paste0("v", vermax)
+    as.character(max(binman::sem_ver(geckover)))
   }else{
     mtch <- match(version, geckover)
     if(is.na(mtch) || is.null(mtch)){
