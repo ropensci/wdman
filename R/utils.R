e50eaf98da85541b2d29e46ee58e76a0f7d7f70c
johndharrison <johndharrison0@gmail.com>
2016-12-19 06:29:11 -0800

    Add utility function to read from process and append to log.

--- R/utils.R
+++ R/utils.R
@@ -3,3 +3,10 @@ os_arch <- function(string = ""){
   arch <- c("32", "64")[.Machine$sizeof.pointer/4]
   paste0(string, arch)
 }
+
+infun_read <- function(handle, env, pipe = PIPE_BOTH, timeout = 0L){
+  msg <- subprocess::process_read(handle, pipe = pipe, timeout = timeout)
+  env[["stdout"]] <- c(env[["stdout"]], msg[["stdout"]])
+  env[["stderr"]] <- c(env[["stderr"]], msg[["stderr"]])
+  msg
+}
