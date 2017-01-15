326aebda9aefcfe2f0ab1c7a18dab6fa7a196699
johndharrison <johndharrison0@gmail.com>
2017-01-15 05:36:29 -0800

    Add tests for assertions

---
+++ tests/testthat/test-assertions.R
@@ -0,0 +1,23 @@
+context("test-assertions")
+
+test_that("assertionsWork", {
+  expect_error(assertthat::assert_that(is_list("")), "is not a list")
+  expect_error(assertthat::assert_that(is_env("")),
+               "is not an environment")
+  expect_error(assertthat::assert_that(is_list_of_df("")),
+               "is not a list of data.frames")
+  expect_error(assertthat::assert_that(is_url("")),
+               "is not a url")
+  expect_error(assertthat::assert_that(is_file("")),
+               "is not a file")
+  expect_error(assertthat::assert_that(is_URL_file("")),
+               "is not a URL or file")
+  expect_error(assertthat::assert_that(is_character(1L)),
+               "should be an character vector")
+  expect_error(assertthat::assert_that(is_data.frame(1L)),
+               "should be a data.frame")
+  expect_error(assertthat::assert_that(contains_required("", "nothere")),
+               "does not have one of")
+  expect_error(assertthat::assert_that(app_dir_exists("")),
+               "app directory not found")
+})
