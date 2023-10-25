564c932627584ce4126749746e2134b960ee1674
Ashby Thorpe <ashbythorpe@gmail.com>
2023-10-25 05:31:57 +0100

    Use rlang::try_fetch() to display errors

--- inst/etc/checks.R
+++ inst/etc/checks.R
@@ -2,47 +2,77 @@ library(wdman)
 
 tries <- list()
 
-tries$chrome <- tryCatch({
-  print("chrome")
-  tmp <- chrome(verbose = TRUE)
-  print(tmp$log())
-  tmp$stop()
-  tmp
-}, error = function(e) {print(e$message); e})
-
-tries$gecko <- tryCatch({
-  print("gecko")
-  tmp <- gecko(verbose = TRUE)
-  print(tmp$log())
-  tmp$stop()
-  tmp
-}, error = function(e) {print(e$message); e})
-
-tries$phantomjs <- tryCatch({
-  print("phantomjs")
-  tmp <- phantomjs(verbose = TRUE)
-  print(tmp$log())
-  tmp$stop()
-  tmp
-}, error = function(e) {print(e$message); e})
-
-if (Sys.info()[["sysname"]] == "Windows") {
-  tries$iedriver <- tryCatch({
-    print("iedriver")
-    tmp <- iedriver(verbose = TRUE)
+tries$chrome <- rlang::try_fetch(
+  {
+    print("chrome")
+    tmp <- chrome(verbose = TRUE)
     print(tmp$log())
     tmp$stop()
     tmp
-  }, error = function(e) {print(e$message); e})
+  },
+  error = function(e) {
+    print(e)
+    e
+  }
+)
+
+tries$gecko <- tryCatch(
+  {
+    print("gecko")
+    tmp <- gecko(verbose = TRUE)
+    print(tmp$log())
+    tmp$stop()
+    tmp
+  },
+  error = function(e) {
+    print(e$message)
+    e
+  }
+)
+
+tries$phantomjs <- tryCatch(
+  {
+    print("phantomjs")
+    tmp <- phantomjs(verbose = TRUE)
+    print(tmp$log())
+    tmp$stop()
+    tmp
+  },
+  error = function(e) {
+    print(e$message)
+    e
+  }
+)
+
+if (Sys.info()[["sysname"]] == "Windows") {
+  tries$iedriver <- tryCatch(
+    {
+      print("iedriver")
+      tmp <- iedriver(verbose = TRUE)
+      print(tmp$log())
+      tmp$stop()
+      tmp
+    },
+    error = function(e) {
+      print(e$message)
+      e
+    }
+  )
 }
 
-tries$selenium <- tryCatch({
-  print("selenium")
-  tmp <- selenium(verbose = TRUE)
-  print(tmp$log())
-  tmp$stop()
-  tmp
-}, error = function(e) {print(e$message); e})
+tries$selenium <- tryCatch(
+  {
+    print("selenium")
+    tmp <- selenium(verbose = TRUE)
+    print(tmp$log())
+    tmp$stop()
+    tmp
+  },
+  error = function(e) {
+    print(e$message)
+    e
+  }
+)
 
 errors <- sapply(tries, function(x) is(x, "simpleError"))
 if (any(errors)) {
